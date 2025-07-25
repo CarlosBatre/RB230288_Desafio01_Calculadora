21d95443403c42e8156ffb499e5bee7b474a593f
RB230288 <carlosbatres.gg@gmail.com>
2025-07-25 13:36:32 -0600

    Inicializacion de variables, declaracion de metodos a utilizar

--- app/src/main/java/udb/edu/sv/desafio01_calculadora/MainActivity.kt
+++ app/src/main/java/udb/edu/sv/desafio01_calculadora/MainActivity.kt
@@ -1,34 +1,52 @@
 package udb.edu.sv.desafio01_calculadora
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
 import android.widget.Switch
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var etMontoTotal: EditText
     private lateinit var etNumPersonas: EditText
     private lateinit var rgPropina: RadioGroup
     private lateinit var etPropinaPersonalizada: EditText
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     private lateinit var switchIVA: Switch
     private lateinit var btnCalcular: Button
     private lateinit var btnLimpiar: Button
     private lateinit var tvResultado: TextView
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        etMontoTotal = findViewById(R.id.etMontoTotal)
+        etNumPersonas = findViewById(R.id.etNumPersonas)
+        rgPropina = findViewById(R.id.rgPropina)
+        etPropinaPersonalizada = findViewById(R.id.etPropinaPersonalizada)
+        switchIVA = findViewById(R.id.switchIVA)
+        btnCalcular = findViewById(R.id.btnCalcular)
+        btnLimpiar = findViewById(R.id.btnLimpiar)
+        tvResultado = findViewById(R.id.tvResultado)
+
+        rgPropina.setOnCheckedChangeListener { _, checkedId ->
+            etPropinaPersonalizada.visibility =
+                if (checkedId == R.id.rbOtro) EditText.VISIBLE else EditText.GONE
+        }
+
+        // Botón Calcular
+        btnCalcular.setOnClickListener {
+            calcularPropina()
+        }
+
+        // Botón Limpiar
+        btnLimpiar.setOnClickListener {
+            limpiarCampos()
         }
     }
 }
